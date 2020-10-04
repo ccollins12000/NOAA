6de5a08c8cc0ccade7627877b4f728ef3ddf795b
Charles Collins <coll5631@stthomas.edu>
2020-10-04 16:48:39 -0500

    Added loop for retrieving all data at a station
    Retrieve min and max date from station and loop through all years. Also fixed bug in paging when no results are returned. Doesn't trying to execute paging if there is no data.

--- NOAA.py
+++ NOAA.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pandas as pd
 import loop_result
+import datetime
+import math
 
 token = input('Enter your NOAA token: ')
 
@@ -35,22 +37,36 @@ alabama_locations = pd.DataFrame(parse_response['results'])
 formatted_reponse = json.dumps(parse_response, indent=3, sort_keys=True)
 
 location = alabama_locations.iloc[0,:]
-# location['mindate']
-# location['maxdate']
+
+mindate = datetime.datetime.strptime(location['mindate'], '%Y-%m-%d')
+maxdate = datetime.datetime.strptime(location['maxdate'], '%Y-%m-%d')
+years = math.ceil((maxdate - mindate).days/365)
+
+start_year = int(mindate.strftime('%Y'))
+all_years = [start_year + index for index in range(years)]
+
 params = {
     'datatypeid':['TMIN','TMAX','TAVG'],
     'stationid': location['id'],
     'datasetid':datasetid,
-    'startdate': '2019-01-01',
-    'enddate': '2019-12-31'
+    'limit':200
 }
+all_weather = []
+for year in all_years:
+    params.update({
+        'startdate':str(year)+'-01-01',
+        'enddate':str(year)+'-12-31'
+    })
+    print(year)
+    all_weather.extend(loop_result.get_all_results(
+        url='https://www.ncdc.noaa.gov/cdo-web/api/v2/data/',
+        headers=headers,
+        parameters=params
+    ))
 
-all_weather = loop_result.get_all_results(
-    url='https://www.ncdc.noaa.gov/cdo-web/api/v2/data/',
-    headers=headers,
-    parameters=params
-)
-print(all_weather)
+all_weather = pd.DataFrame(all_weather)
+
+print(all_weather.head())
 # r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data/', headers = headers, params = params)
 
 def process_json(json_text):
--- loop_result.py
+++ loop_result.py
@@ -7,20 +7,26 @@ def get_all_results(url, headers, parameters):
     results = []
     while True:
         # request data
+        parameters = parameters.copy()
         response = requests.get(url, headers=headers, params=parameters)
-
+        initial_limit = parameters.get('limit', 0)
         # parse out data from response
         parsed_response = json.loads(response.text)
         metadata = parsed_response.get('metadata', {}).get('resultset', {})
+
+        if not parsed_response.get('results', []):
+            print('no results for: ', parameters)
+            break
+
         results.extend(parsed_response.get('results', []))
 
         # check record counts
         record_count = metadata.get('count')
         limit = metadata.get('limit')
         pages = math.ceil(record_count/limit)
-        current_page = metadata.get('offset')
+        current_page = metadata.get('offset', 0)
 
-        print('Page ', current_page, ' of ', current_page, sep='')
+        print('Page ', current_page, ' of ', pages, sep='')
         # setup next iteration
         if not record_count or current_page >= pages:
             break
@@ -29,6 +35,4 @@ def get_all_results(url, headers, parameters):
         time.sleep(0.5)
 
     return results
-
-
 
