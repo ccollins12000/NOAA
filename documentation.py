dbc097c9eddb2e273181ad9e917805004f0e7d47
Charles Collins <coll5631@stthomas.edu>
2020-10-04 12:00:02 -0500

    Added links to reference pages

---
+++ documentation.py
@@ -0,0 +1,8 @@
+# List of webservices:
+# https://www.ncdc.noaa.gov/cdo-web/webservices
+
+# Get token
+# https://www.ncdc.noaa.gov/cdo-web/token
+
+# Getting Started
+# https://www.ncdc.noaa.gov/cdo-web/webservices/v2
